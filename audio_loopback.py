e09691ad55512b0c562106b59d2c375e3efcc6c5
Ata <ahakcil@gmail.com>
2021-01-24 19:19:39 +0300

    Added better logging

--- audio_loopback.py
+++ audio_loopback.py
@@ -14,12 +14,14 @@ class AudioController:
         self.CHUNK = 1024
         self.device_name = device_name
         self.p = pyaudio.PyAudio()
+        
+        self.last = []
 
         #open audio 
+        logging.pprint("Starting audio stream", 4)
         self.stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS,
             rate=self.RATE, input=True, input_device_index=self.find_device(device_name),
             frames_per_buffer=self.CHUNK)
-        self.last = []
 
         self.stream.start_stream()
 
@@ -27,6 +29,7 @@ class AudioController:
     def __del__(self):
         """ Stop stream and close. Terminate pyaudio.
         """
+        logging.pprint("Stopping audio stream", 2)
         self.stream.stop_stream()
         self.stream.close()
         self.p.terminate()
@@ -40,9 +43,12 @@ class AudioController:
         sumt = 0
         for j in range(count):
             for k in range(reduction_coef):
-                sumt+=dfft[count*j+k]
+                if(dfft[count*j+k]>0):
+                    sumt+=dfft[count*j+k]
             simplified.append(sumt)
             sumt=0
+            
+        logging.pprint(f"Simplified FFT: {simplified}", 6)
         return simplified
 
 
@@ -56,7 +62,7 @@ class AudioController:
         for i in range(0, numdevices):
                 #print all device names
                 if (self.p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-                    print("Input Device id ", i, " - ", self.p.get_device_info_by_host_api_device_index(0, i).get('name'))
+                    logging.pprint(f"Input Device id {i} - {self.p.get_device_info_by_host_api_device_index(0, i).get('name')}", )
 
         for i in range(self.p.get_device_count()):
             # check if device name starts with expected device name
@@ -67,14 +73,17 @@ class AudioController:
         """ Read data from stream but only once.
         """
         # Read chunk sized data from stream
+        logging.pprint("Reading data...", 6)
         in_data = self.stream.read(self.CHUNK)
 
         # Format audio data
+        logging.pprint("Formatting data...", 6)
         audio_data = np.fromstring(in_data, np.int16)
         
         try:
             # Fast Fourier Transform, 10*log10(abs) is to scale it to dB
             # and make sure it's not imaginary
+            logging.pprint("Performing FFT...", 6)
             dfft = 10.*np.log10(abs(np.fft.rfft(audio_data)))
         except:
             # if something went wrong (most likely division by 0) send last output and hope it doesn't happen again.
